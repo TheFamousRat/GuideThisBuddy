86d2ce0ed340c7e3add7ed6084c51c7d35fc99fd
Guillaume Roy <gpotato.gr@gmail.com>
2018-12-20 03:22:04 +0100

    Adding missing folders

---
+++ godot/Platforms/Sucker/Sucker.gd
@@ -0,0 +1,13 @@
+extends Spatial
+
+var suckForce : Vector3 = Vector3(0,0,0)
+var suckStrength : float = 1000.0
+
+func _ready():
+	suckForce = (self.get_global_transform().origin - $Position3D.get_global_transform().origin).normalized()
+
+func _process(delta):
+	var bodies = $Area.get_overlapping_bodies()
+	for i in bodies:
+		if i is RigidBody:
+			i.add_force(suckForce * delta * suckStrength, Vector3(0,0,0))
