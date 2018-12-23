cb3b768062ae8733728d9ac31f94f4a67d67f3eb
Guillaume Roy <gpotato.gr@gmail.com>
2018-12-23 18:32:13 +0100

    Updated Bumper
    Bumper push now directional relative to the Bumper's rotation
    
    Push force is now modifiable (maybe Pusher plater can be a Bumper but weaker ?
    
    Made things great again

--- godot/Platforms/Bumper/Bumper.gd
+++ godot/Platforms/Bumper/Bumper.gd
@@ -2,6 +2,7 @@ extends Spatial
 
 export (float) var extensionTime = 0.01 #Time it takes the bumper to fully extend
 export (float) var extendedStateDuration  = 2.0 #Time the bumper spends extended
+export (float) var pushForce = 20.0
 
 var open : bool = false
 
@@ -10,5 +11,8 @@ func _ready():
 
 	$ExtendedTimer.wait_time = extendedStateDuration
 
-func _on_Area_rigidBodyDetected():
-	$Tween.set_active(true)
+func _process(delta):
+	var bodies = $Area.get_overlapping_bodies()
+	for i in bodies:
+			i.add_force(pushForce*(($Position3D.get_global_transform().origin - self.get_global_transform().origin).normalized()),Vector3(0,0,0))
+			$Tween.set_active(true)
