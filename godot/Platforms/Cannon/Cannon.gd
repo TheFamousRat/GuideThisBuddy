56bfa4943aec51776bb9726af6481a574e1c8c33
Guillaume Roy <gpotato.gr@gmail.com>
2018-12-24 17:27:34 +0100

    Cannon now functional

--- godot/Platforms/Cannon/Cannon.gd
+++ godot/Platforms/Cannon/Cannon.gd
@@ -1,13 +1,10 @@
 extends Spatial
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+export (float) var cannonForce = 300.0
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	var bodies = $BodyDetector.get_overlapping_bodies()
+	for i in bodies:
+		if i is RigidBody:
+			i.add_central_force(cannonForce*(($Position3D.get_global_transform().origin - self.get_global_transform().origin).normalized()))
+			print("Boom")
