5a2e989e667b461fcd7524eee8acc35d33e1cebe
Guillaume Roy <gpotato.gr@gmail.com>
2018-12-25 16:46:34 +0100

    Updated Cannon
    Added a shader
    
    Added an animation for detonation (maybe visual effects will be added later on ?)
    
    Now a RigidBody has a force applied to it by the Cannon only once (it can of course go in the same Cannon again, it just has to leave it and re-enter it)
    
    hasAGirlfriend() still returns false for some weird reason

--- godot/Platforms/Cannon/Cannon.gd
+++ godot/Platforms/Cannon/Cannon.gd
@@ -1,10 +1,16 @@
 extends Spatial
 
-export (float) var cannonForce = 300.0
+export (float) var cannonForce = 2000.0
+
+var propulsedBodies : Array
+
+func _ready():
+	propulsedBodies = Array()
 
 func _process(delta):
 	var bodies = $BodyDetector.get_overlapping_bodies()
 	for i in bodies:
-		if i is RigidBody:
+		if i is RigidBody and !propulsedBodies.has(i.get_path()):
 			i.add_central_force(cannonForce*(($Position3D.get_global_transform().origin - self.get_global_transform().origin).normalized()))
-			print("Boom")
+			propulsedBodies.append(i.get_path())
+			$AnimationPlayer.play("explosion")
