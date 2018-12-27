d15a66b9da2a6e2230bff303d43a555efb353b83
Guillaume Roy <gpotato.gr@gmail.com>
2018-12-27 04:01:42 +0100

    Basis of level system
    Base class of level system, from which all other levels will inherit
    
    For now, the (basic) functionalities are : setting a starting point, one or more areas of arrival, the available platers as well as their available numbers
    
    This class is however not linked with the Main Node for now, which will be done in a few hours
    
    getHuman(Guillaume).getSleepTime() still far too low

---
+++ godot/LevelBase/LevelBase.gd
@@ -0,0 +1,49 @@
+tool
+
+extends Node
+
+export (Array) var availablePlaters setget availablePlaterArrayChecker
+
+"""
+To do a new level, one has to do the following :
+	1/Create new scene inherited from LevelBase
+	2/Set the "available platers" array. It functions
+	like that : the size of the array is always even, 
+	because the instructions are given 2-by-2.
+	In a couple, the first cell is always of type
+	Object, while the second is of type int.
+	The first loads the plater and his whole scene,
+	while the int gives the number of such platers
+	that can be placed in the level.
+	3/Do the layout as you please...
+	4/Don't forget to give the player arrival area at 
+	least one collisionShape !
+	5/You're good to go
+	
+	Note :
+		If you want to personalize the script of your
+		level, don't forget to inherit it from this one,
+		don't touch it directly...
+"""
+
+"""
+Needed variables :
+	Level layout
+"""
+
+func availablePlaterArrayChecker(newArray : Array):
+	if newArray != null:
+		if newArray.size() % 2 == 1 && newArray.size() > availablePlaters.size():
+			availablePlaters = newArray.duplicate()
+			availablePlaters.append(int(0))
+		elif newArray.size() % 2 == 1 && newArray.size() < availablePlaters.size():
+			availablePlaters = newArray.duplicate()
+			availablePlaters.remove(availablePlaters.size() - 1)
+		else:
+			availablePlaters = newArray.duplicate()
+		
+		for i in availablePlaters.size():
+			if i%2 == 0 and !(availablePlaters[i] is Object):
+				availablePlaters[i] = Object()
+			elif i%2 == 1 and !(availablePlaters[i] is int):
+				availablePlaters[i] = int(0)
