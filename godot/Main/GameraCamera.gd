d8f95db25e3b6f6c1157c35bed1b569c4914b753
Guillaume Roy <gpotato.gr@gmail.com>
2019-01-04 01:53:54 +0100

    Added a TestRoom

--- godot/Main/GameraCamera.gd
+++ godot/Main/GameraCamera.gd
@@ -5,6 +5,10 @@ export (float) var targetDist
 
 var target : Spatial setget setTarget
 
+func _ready():
+	self.projection = Camera.PROJECTION_ORTHOGONAL
+	self.size = targetDist
+
 func _process(delta):
 	self.set_translation(target.get_global_transform().origin + distVect * targetDist)
 
