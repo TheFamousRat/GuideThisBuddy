984486a80ab8406b2bdee9b0b8af09152ff7c7ee
Guillaume Roy <gpotato.gr@gmail.com>
2019-01-04 19:02:04 +0100

    Clarified Cannon script

--- godot/Platers/Cannon/Cannon.gd
+++ godot/Platers/Cannon/Cannon.gd
@@ -1,28 +1,8 @@
-tool
-
 extends "res://Platers/PlaterBase/PlaterBase.gd"
 
 export (float) var cannonForce = 2000.0
 
-var propulsedBodies : Array
-
-func _ready():
-	propulsedBodies = Array()
-
-func _process(delta):
-	var bodies = $BodyDetector.get_overlapping_bodies()
-	for i in bodies:
-		if i is RigidBody and !propulsedBodies.has(i.get_path()):
-			i.add_central_force(cannonForce*(($Position3D.get_global_transform().origin - self.get_global_transform().origin).normalized()))
-			propulsedBodies.append(i.get_path())
-			$AnimationPlayer.play("explosion")
-			
-	var foundPath : bool = false
-	for i in propulsedBodies:
-		foundPath = false
-		for j in bodies:
-			if j.get_path() == i:
-				foundPath = true
-				break
-		if !foundPath:
-			propulsedBodies.erase(i)
+func _on_BodyDetector_body_entered(body):
+	if body.is_in_group("player"):
+		body.add_central_force(cannonForce*(($Position3D.get_global_transform().origin - self.get_global_transform().origin).normalized()))
+		$AnimationPlayer.play("explosion")
