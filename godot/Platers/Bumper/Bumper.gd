fdfc6bf0f6c808a7770f0a8c98ba82532d0fd959
Guillaume Roy <gpotato.gr@gmail.com>
2019-01-04 04:20:15 +0100

    Completed Bumper node
    Player now correctly bumps over the plater : it acts roughly like a trampoline
    
    Problem about applying the force only once

--- godot/Platers/Bumper/Bumper.gd
+++ godot/Platers/Bumper/Bumper.gd
@@ -1,7 +1,38 @@
 extends "res://Platers/PlaterBase/PlaterBase.gd"
 
+var bumpStrength : float = 100.0
+var launchedBodies : Array
+
 func _process(delta):
 	var bodies = $BodyDetector.get_overlapping_bodies()
 	for i in bodies:
-		if i.is_in_group("player"):
-			pass
+		if i.is_in_group("player") and !launchedBodies.has(i.get_path()):
+			var xVec : Vector3
+			var yVec : Vector3
+			var zVec : Vector3
+			xVec = $Spatial/xAxis.get_global_transform().origin - $Spatial/origin.get_global_transform().origin
+			yVec = $Spatial/yAxis.get_global_transform().origin - $Spatial/origin.get_global_transform().origin
+			zVec = $Spatial/zAxis.get_global_transform().origin - $Spatial/origin.get_global_transform().origin
+			i.add_force(bumpStrength * bounceVect(i.linear_velocity,xVec,yVec,zVec),Vector3(0,0,0))
+			launchedBodies.append(i.get_path())
+			
+func bounceVect(inputVec : Vector3, xAxis : Vector3, yAxis : Vector3, zAxis : Vector3):
+	var a : float = xAxis.x
+	var b : float = xAxis.y
+	var c : float = xAxis.z
+	var d : float = yAxis.x
+	var e : float = yAxis.y
+	var f : float = yAxis.z
+	var g : float = zAxis.x
+	var h : float = zAxis.y
+	var i : float = zAxis.z
+	var k : float = inputVec.x
+	var l : float = inputVec.y
+	var m : float = inputVec.z
+	
+	var beta : float
+	
+	#yum
+	beta = (a*l-k*b+(m*(a*e-d*b)+c*(l*d-k*e)+f*(k*b-a*l)) / (c*(h*d-g*e)+f*(g*b-h*a)-i)*(g*b-h*a))/(d*b-a*e)
+	
+	return (2.0 * (beta * yAxis) + inputVec)
--- godot/TestRoom/TestRoom.gd
+++ godot/TestRoom/TestRoom.gd
@@ -2,14 +2,33 @@ tool
 
 extends Node
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+func _process(delta):
+	var xVec : Vector3
+	var yVec : Vector3
+	var zVec : Vector3
+	xVec = $Spatial/xAxis.get_global_transform().origin - $Spatial/origin.get_global_transform().origin
+	yVec = $Spatial/yAxis.get_global_transform().origin - $Spatial/origin.get_global_transform().origin
+	zVec = $Spatial/zAxis.get_global_transform().origin - $Spatial/origin.get_global_transform().origin
+	$u.translation = $i.translation + $i.cast_to
+	$u.cast_to = bounceVect($i.cast_to,xVec,yVec,zVec)
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func bounceVect(inputVec : Vector3, xAxis : Vector3, yAxis : Vector3, zAxis : Vector3):
+	var a : float = xAxis.x
+	var b : float = xAxis.y
+	var c : float = xAxis.z
+	var d : float = yAxis.x
+	var e : float = yAxis.y
+	var f : float = yAxis.z
+	var g : float = zAxis.x
+	var h : float = zAxis.y
+	var i : float = zAxis.z
+	var k : float = inputVec.x
+	var l : float = inputVec.y
+	var m : float = inputVec.z
+	
+	var beta : float
+	
+	#yum
+	beta = (a*l-k*b+(m*(a*e-d*b)+c*(l*d-k*e)+f*(k*b-a*l)) / (c*(h*d-g*e)+f*(g*b-h*a)-i)*(g*b-h*a))/(d*b-a*e)
+	
+	return (2.0 * (beta * yAxis) + inputVec)
