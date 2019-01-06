27c5f6685b003c4a362640bd065622253c7de28f
Guillaume Roy <gpotato.gr@gmail.com>
2019-01-06 17:28:47 +0100

    Cleaned up level loading process
    Mainly ousting the 'currentLevel' node

--- godot/nodes/Main/Main.gd
+++ godot/nodes/Main/Main.gd
@@ -9,13 +9,9 @@ func _ready():
 	$GUI.loadGui($GUI.mainMenu)
 
 func loadLevel(levelPath : String):
-	#Unloading the previous level
-	while $CurrentLevel.get_child_count() != 0:
-		$CurrentLevel.remove_child($CurrentLevel.get_child(0))
-	
-	#Loading the level and adding it as a child
+	#Loading the new level and adding it as a child
 	var currentLevel = load(levelPath).instance()
-	$CurrentLevel.add_child(currentLevel)
+	self.add_child(currentLevel)
 	
 	#Applying the parameters of the level
 	$Slime.translation = currentLevel.get_node("PlayerStart").translation
@@ -24,13 +20,13 @@ func loadLevel(levelPath : String):
 	$Slime.show()
 	$GUI.loadGui($GUI.platerPlacement)
 	$GameCamera.centerOn($Slime)
-	
+
 func launchLevel():
 	$Slime.set_sleeping(false)
 	$GameCamera.setTarget($Slime)
 	
 func getLevelAvailablePlaters():
-	return $CurrentLevel
+	return currentLevel
 	
 func _process(delta):
 	pass
