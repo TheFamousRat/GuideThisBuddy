935cc03b986109c34ddae0eb5e98a2141640bf0b
Guillaume Roy <gpotato.gr@gmail.com>
2019-01-10 03:19:09 +0100

    Added a menu item to quit game
    And an overly pretentious one, at that

--- godot/nodes/GUI/MainMenu/MainMenu.gd
+++ godot/nodes/GUI/MainMenu/MainMenu.gd
@@ -1,5 +1,13 @@
 extends Control
 
-func _on_Button_pressed():
+func _on_Start_pressed():
 	get_node(Global.mainPath).loadLevel("res://nodes/Levels/TestLevel/TestLevel.tscn")
 	self.queue_free()
+
+
+func _on_Quit_pressed():
+	$ConfirmQuit.popup()
+
+
+func _on_ConfirmQuit_confirmed():
+	get_tree().quit()
