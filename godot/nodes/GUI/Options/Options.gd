15ed8a1bf20393185e88eb4f50f1e751d361bde5
Guillaume Roy <gpotato.gr@gmail.com>
2019-01-29 19:16:15 +0100

    Added possibility to change MSAA

--- godot/nodes/GUI/Options/Options.gd
+++ godot/nodes/GUI/Options/Options.gd
@@ -7,4 +7,14 @@ func _on_Quit_pressed():
 	self.queue_free()
 
 func _on_Antialiasing_item_selected(ID):
-	get_viewport().set_antiali
+	match $Antialiasing.get_item_text(ID):
+		"Disabled":
+			get_viewport().msaa = Viewport.MSAA_DISABLED
+		"x2":
+			get_viewport().msaa = Viewport.MSAA_2X
+		"x4":
+			get_viewport().msaa = Viewport.MSAA_4X
+		"x8":
+			get_viewport().msaa = Viewport.MSAA_8X
+		"x16":
+			get_viewport().msaa = Viewport.MSAA_16X
