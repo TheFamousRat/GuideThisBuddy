7074d0fe7858e850bf2f48895554b8e04c1183d5
Guillaume Roy <gpotato.gr@gmail.com>
2019-02-17 04:58:35 +0100

    Updated Cannon with new model

--- godot/nodes/Platers/Cannon/Cannon.gd
+++ godot/nodes/Platers/Cannon/Cannon.gd
@@ -2,6 +2,10 @@ extends "res://nodes/Platers/PlaterBase/PlaterBase.gd"
 
 export (float) var cannonForce = 100.0
 
+func clone():
+	var ret = self.duplicate()
+	return ret
+
 func _on_BodyDetector_body_entered(body):
 	if body is RigidBody and !disabled:
 		body.set_global_transform($Position3D.get_global_transform())
